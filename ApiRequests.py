fbd80fb8746929b2ce717aff3e8724a7c4d13b3f
Spencer <Spencer.vantrease@gmail.com>
2021-12-20 17:46:13 -0600

    Some refactoring and additions

--- ApiRequests.py
+++ ApiRequests.py
@@ -3,27 +3,29 @@ import json
 from time import sleep
 
 class APIRequests:
+    
+    UrlDict = {'base':'https://api.helium.io/v1/', 'hotspot':'https://api.helium.io/v1/hotspots/', 'activity':'https://api.helium.io/v1/hotspots/~/activity?'}
+    
     def __init__(self, Address):
-        self.UrlDict = {'base':'https://api.helium.io/v1/', 'hotspot':'https://api.helium.io/v1/hotspots/', 'activity':'https://api.helium.io/v1/hotspots/~/activity?'}
         self.Address = Address
-        # self.Stats = requests.get("https://api.helium.io/v1/stats")
         self.status = None
     
-    def GetHotspotInfo(self, dictUrl = 'base', params = None , event = None):        
+    def GetHotspotInfo(self, dictUrl = 'hotspot', params = None , event = None):        
         h1 = requests.get(f"{self.UrlDict[dictUrl]}{self.Address}", params = params)
         if h1.status_code == 429:
             sleep(int(h1.headers['retry-after'])+1)
-            self.GetHotspotInfo()
+            return self.GetHotspotInfo()
         self.rJson1 = json.loads(h1.content)['data'] if json.loads(h1.content)['data'] else {'data':{'timestamp_added': 0}}
         return self.rJson1
         
-    def GetHotspotActivity(self, cursor = None, filters = None, event = None):
+    def GetHotspotActivity(self, cursor = None, params = None, event = None):
         if not cursor:
-            h2 = requests.get(f"{self.UrlDict['hotspot']}{self.Address}/activity?", filters)
-            cursor = json.loads(h2.content)['cursor']
+            h2 = requests.get(f"{self.UrlDict['hotspot']}{self.Address}/activity?", params=params)
+            data = json.loads(h2.content)
+            cursor = data['cursor']
         h2 = requests.get(f"{self.UrlDict['hotspot']}{self.Address}/activity?{cursor}")
         self.rJson2 = json.loads(h2.content)
-        return self.rJson2
+        return data,self.rJson2 if data['data'] else self.rJson2
     def GetHotspotLocation(self, distance = "15000", event = None):
         h3 = requests.get(f"{self.UrlDict['hotspot']}location/distance?lat={self.rJson1['lat']}&lon={self.rJson1['lon']}&distance={distance}")
 
--- DataPull.py
+++ DataPull.py
@@ -3,66 +3,80 @@ import csv
 from datetime import datetime, timedelta
 from pathlib import Path
 
+class Pulling:
+    rewcount = 0
+    file = Path(input("Input csv file path. Either absolute or relative to script execution: ")).resolve()
 
-def AddDays(date):
-    mintime=datetime.strptime(date[0:10], "%Y-%m-%d")
-    return mintime + timedelta(1)
+    def AddDays(self, date, days=1):
+        mintime=datetime.strptime(date[0:10], "%Y-%m-%d")
+        return (mintime + timedelta(days)).isoformat()[0:10]
 
 
-def addresses(addressfield = 'Address', retfields = False):
-    with open(file := Path(input("Input csv file path. Either absolute or relative to script execution.")).resolve(), 'r', encoding='utf-8-sig') as csvfile:
-        reader = csv.DictReader(csvfile)
-        # if retfields is True:
-        #     return reader.fieldnames 
-        # if addressfield not in reader.fieldnames:
-        #     return f"File does not contain a field called {addressfield}. Pass fieldname as addressfield param or input correct csv file."
-        
-
-        # This return statement may work, have not tested need to test this tmrw. 12/17
-        return [x for x in reader.fieldnames] if retfields is True else f"File does not contain a field called {addressfield}. Pass fieldname as addressfield param or input correct csv file." if addressfield not in reader.fieldnames else [x[addressfield] for x in reader]
+    def addresses(self, addressfield = 'Address', retfields = False):
+        with open(self.file, 'r', encoding='utf-8-sig') as csvfile:
+            reader = csv.DictReader(csvfile)
+            return [x for x in reader.fieldnames] if retfields is True else f"File does not contain a field called {addressfield}. Pass fieldname as addressfield param or input correct csv file." if addressfield not in reader.fieldnames else [x[addressfield] for x in reader]
         
         
-def atb(request):
-        # request = ApiRequests.APIRequests(address)
+    def atb(self, request):
+        # Gets Date Added to Blockchain
         print(request.GetHotspotInfo(), "\n")
-        if not (atb := request.GetHotspotInfo()['timestamp_added']):
-            return("Request for hotspot info failed.")
-        return atb[0:10] if not atb else 0
+        if not (atbdata := request.GetHotspotInfo()['timestamp_added']):
+            return 0
+        self.atbdata = atbdata[0:10]
+        return self.atbdata
         
 
-def firstreward(request):
-    request.GetHotspotActivity()
-    if not (rew := request.GetHotspotActivity()):
-        pass
+    def firstreward(self, request):
+        # Gets Date of First Reward
+        params = {'min_time': self.atbdata, 'max_time': self.AddDays(self.atbdata, days=(self.rewcount + 1)), 'filter_types': ',rewards_v1,rewards_v2', 'limit': 100 }
+        if self.rewcount >= 20:
+            self.rewcount = 0
+            print('No Reward activity within 20 days of being adding to blockchain. \n')
+            return 0
+        if not (rew := request.GetHotspotActivity(params=params)):
+            return 0
+        if type(rew) is not tuple:
+            if not rew['data']:
+                self.rewcount += 1
+                return self.firstreward()
+            return rew['data'][len(rew['data']) - 1]['time']
+        if rew[1]['data']:
+            rew = rew[1]
+            return rew['data'][len(rew['data']) - 1]['time']
+        rew = rew[0]
+        return rew['data'][len(rew['data']) - 1]['time']
     
 
-def firstdatatransfer(request):
-    request.GetHotspotActivity()
+    def firstdatatransfer(self, request):
+        request.GetHotspotActivity()
 
-def firstpoc(request):
-    request.GetHotspotActivity()
+    def firstpoc(self, request):
+        request.GetHotspotActivity()
 
 
-def allassert(request):
-    request.GetHotspotActivity()
+    def allassert(self, request):
+        request.GetHotspotActivity()
 
 
 def main():
-    fieldnamelist = addresses()
+    pullobject = Pulling()
+    fieldnamelist = pullobject.addresses(retfields=True)
     
     with open('Hotspot.csv', 'a', encoding='utf-8-sig') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnamelist)
         writer.writeheader()
 
-        for i in addresses('Hotspot Name'):
+        for i in pullobject.addresses('Hotspot Name'):
             if not i:
                 continue
             request = ApiRequests.APIRequests(i)
             # need to slow this down possibly restricted to 1 request a second maybe 100 a minute then 1 per however long
             # csvwritedict = dict(zip(fieldnamelist,[i, atb(request), firstreward(request), firstdatatransfer(request), firstpoc(request), allassert(request)]))
-            csvwritedict = dict(zip(fieldnamelist,[i, atb(request)]))
-            writer.writerow(csvwritedict)
+            # csvwritedict = dict(zip(fieldnamelist,[i, atb(request)]))
+            print(pullobject.atb(request), pullobject.firstreward(request))
+            # writer.writerow(csvwritedict)
 
 if __name__ == '__main__':
-    # main()
-    print(addresses('Hotspot Name', True))
+    main()
+    # print(Pulling.addresses(retfields=True))
